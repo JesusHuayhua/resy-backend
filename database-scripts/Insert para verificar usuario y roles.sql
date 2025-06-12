0cd3581279e5724f2a4a83cdb6db9957fba36468
Saya <crackmekun@protonmail.com>
2025-06-11 20:13:23 -0500

    s

--- database-scripts/Insert para verificar usuario y roles.sql
+++
@@ -1,6 +0,0 @@
-SET search_path TO 'ResyDB', public;
-
-INSERT INTO "Roles" (nombrerol) values ('Admin'),('Cajero'),('Cliente');
-
-select * from "Roles";
-select * from "Usuario";
--- services/ServicioUsuario/dominio/rol.go
+++
@@ -1,6 +0,0 @@
-package UserModels
-
-type Rol struct {
-	ID        int    `json:"id_rol"`
-	NombreRol string `json:"nombrerol"`
-}
