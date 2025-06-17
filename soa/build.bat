d368676b54782f7fd57a05671b8d26ba480c3a7d
Saya <crackmekun@protonmail.com>
2025-06-17 16:06:25 -0500

    Update build.bat

--- soa/build.bat
+++ soa/build.bat
@@ -4,6 +4,8 @@ if exist go.mod if exist go.sum (
 )
 echo "[BUILD] Building go project"
 go mod init soa
+go get google.golang.org/genproto@latest
+go get google.golang.org/grpc@latest
 go mod tidy
 echo "[BUILD] go project built."
 pause
