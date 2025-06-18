1179e761c92ec844422897101fefb2bfbd737f18
Saya <crackmekun@protonmail.com>
2025-06-17 21:44:56 -0500

    Update main.go

--- soa/services/users/pkg/main.go
+++ soa/services/users/pkg/main.go
@@ -62,7 +62,7 @@ func main() {
 		})
 	}
 	{
-		//NO TOCAR EL PUERTO DE ESTA HUEVADA, ES WIP.
+		//NO TOCAR EL PUERTO DE GRPC, ES WIP. VAS A CRASHEAR LA APLICACION.
 		grpcListener, err := net.Listen("tcp", grpcAddr)
 		if err != nil {
 			logger.Log("transport", "gRPC", "during", "Listen", "err", err)
