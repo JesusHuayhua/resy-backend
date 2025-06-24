503b13b0ced40290bbd56a957bd091c523a90879
Shauanth <a20212602@pucp.edu.pe>
2025-06-24 15:17:29 -0500

    Menora de la ejecucion cambiando .bat por .js (requisito tener instalado node.js)

---
+++ soa/services/ServicioUsuario/build.js
@@ -0,0 +1,30 @@
+// build.js
+const { execSync } = require('child_process');
+const fs = require('fs');
+const readline = require('readline');
+
+try {
+  // Eliminar archivos
+  ['go.mod', 'go.sum'].forEach(file => {
+    if (fs.existsSync(file)) fs.unlinkSync(file);
+  });
+
+  console.log('[BUILD] Building go project');
+  execSync('go mod init ServicioUsuario', { stdio: 'inherit' });
+  execSync('go mod tidy', { stdio: 'inherit' });
+  // Compilar el servicio
+  execSync('go build -o servicio_usuario.exe ./cmd/main.go', { stdio: 'inherit' });
+  console.log('[BUILD] go project built.');
+} catch (error) {
+  console.error('Error:', error.message);
+  process.exit(1);
+}
+
+// Pausa
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('Press Enter to continue...', () => {
+  rl.close();
+});
